f70ef2ef17ee5e1b32dffd81e2f3258c475bba17
Aminul Haque Khan <154525719+aminulhaquekhan@users.noreply.github.com>
2024-01-09 00:07:43 +0600

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -1,7 +1,43 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
-int main()
-{
- return 0;
+class temp{
+    string userName,Email,password;
+    string searchName,searchPass,searchEmail;
+    fstream file;
+    public:
+    void login();
+    void signUP();
+    void forgot();
+}obj;
+
+int main(){
+    char choice;
+    cout<<"\n1- Login";
+    cout<<"\n2- Sign-Up";
+    cout<<"\n3- Forgot Password";
+    cout<<"\n4- Exit";
+    cout<<"\nEnter Your Choice :: ";
+    cin>>choice;
+
+    switch(choice){
+        case '1':
+            cin.ignore();
+            obj.login();
+        break;
+        case '2':
+            cin.ignore();
+            obj.signUP();
+        break;
+        case '3':
+            cin.ignore();
+            obj.forgot();
+        break;
+        case '4':
+            return 0;
+        break;
+        defualt:
+            cout<<"Invalid Selection...!";
+    }
 }
+
